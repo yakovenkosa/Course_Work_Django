a91e7bcda37f230ed78abdcaa4d99687a7445718
Sergey <sergey199433@gmail.com>
2025-01-21 22:21:38 +0400

    fix

--- celery_config.py
+++ celery_config.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from celery_config import Celery
+from celery import Celery
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
